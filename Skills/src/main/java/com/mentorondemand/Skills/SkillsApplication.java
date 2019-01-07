98298cca0644f0dacfe5b059454895a270c0bc72
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-07 13:48:45 +0530

    Commit initial base line project on 7th Jan 2019. Project names are Skills/Trainings and UserProfile

---
+++ Skills/src/main/java/com/mentorondemand/Skills/SkillsApplication.java
@@ -0,0 +1,14 @@
+package com.mentorondemand.Skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsApplication.class, args);
+	}
+
+}
+
---
+++ UserProfile/src/main/java/com/mentorondemand/UserProfile/UserProfileApplication.java
@@ -0,0 +1,14 @@
+package com.mentorondemand.UserProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
+
