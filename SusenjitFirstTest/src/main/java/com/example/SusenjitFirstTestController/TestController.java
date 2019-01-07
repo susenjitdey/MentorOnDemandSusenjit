536cee55d8d8332dd6bdf5b0975f3d8479220994
Susenjit Dey <susenjitdey@gmail.com>
2019-01-07 13:19:36 +0530

    Added First COntroller

---
+++ SusenjitFirstTest/src/main/java/com/example/SusenjitFirstTestController/TestController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.example.SusenjitFirstTestController;
+
+/**
+ * @author ibm
+ *
+ */
+public class TestController {
+
+	/**
+	 * 
+	 */
+	public TestController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
