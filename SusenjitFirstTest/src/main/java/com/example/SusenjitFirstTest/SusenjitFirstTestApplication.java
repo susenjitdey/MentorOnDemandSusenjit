deb47d6b1f85260693555f5192ef2ac14a896925
Susenjit Dey <susenjitdey@gmail.com>
2019-01-07 13:13:06 +0530

    Code Checking: First code base line commit.

---
+++ SusenjitFirstTest/src/main/java/com/example/SusenjitFirstTest/SusenjitFirstTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.SusenjitFirstTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusenjitFirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusenjitFirstTestApplication.class, args);
+	}
+
+}
+
