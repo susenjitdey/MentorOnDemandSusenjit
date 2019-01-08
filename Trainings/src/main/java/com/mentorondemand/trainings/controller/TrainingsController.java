abbf14aebbeb56e862640ca0c677dcc150a8951b
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-08 17:10:19 +0530

    Updated file and pom for DB connectivity.

--- Trainings/src/main/java/com/mentorondemand/trainings/controller/TrainingsController.java
+++ Trainings/src/main/java/com/mentorondemand/trainings/controller/TrainingsController.java
@@ -3,10 +3,17 @@
  */
 package com.mentorondemand.trainings.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mentorondemand.trainings.model.TrainingsEntity;
+
 /**
  * @author javaibm04
  *
  */
 public class TrainingsController {
+	
+	@Autowired
+	TrainingsEntity trainingsEntity;
 
 }
