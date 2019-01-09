193a1ff5f9df457d352ced1815f14b4564c82c40
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-09 20:40:21 +0530

    Modified files.

--- MentorCalendar/src/main/java/com/mentorondemand/MentorCalendar/controller/MentorCalendarController.java
+++ MentorCalendar/src/main/java/com/mentorondemand/MentorCalendar/controller/MentorCalendarController.java
@@ -3,10 +3,17 @@
  */
 package com.mentorondemand.MentorCalendar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mentorondemand.MentorCalendar.model.MentorCalendarEntity;
+
 /**
  * @author javaibm04
  *
  */
 public class MentorCalendarController {
+	
+	@Autowired
+	MentorCalendarEntity mentorCalendarEntity;
 
 }
--- MentorCalendar/src/main/java/com/mentorondemand/MentorCalendar/repositorydao/MentorCalendarDAO.java
+++ MentorCalendar/src/main/java/com/mentorondemand/MentorCalendar/repositorydao/MentorCalendarDAO.java
@@ -3,10 +3,15 @@
  */
 package com.mentorondemand.MentorCalendar.repositorydao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mentorondemand.MentorCalendar.model.MentorCalendarEntity;
+
+
 /**
  * @author javaibm04
  *
  */
-public interface MentorCalendarDAO {
+public interface MentorCalendarDAO extends JpaRepository<MentorCalendarEntity, Long>{
 
 }
