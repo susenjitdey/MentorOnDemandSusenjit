b2b9f8f523530e0e18f00c11f70ec7b3d57db012
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-28 10:22:57 +0530

    New Project Added for Mentor Profile

---
+++ MentorProfile/src/main/java/com/mentorprofile/MentorProfileApplication.java
@@ -0,0 +1,14 @@
+package com.mentorprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorProfileApplication.class, args);
+	}
+
+}
+
