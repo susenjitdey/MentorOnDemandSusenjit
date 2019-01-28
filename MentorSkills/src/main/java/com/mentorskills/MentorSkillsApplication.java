c715d2b6acfd5da5d1206d8689336b7d6e211bd5
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-28 10:22:19 +0530

    New Project added for Mentor Skills

---
+++ MentorSkills/src/main/java/com/mentorskills/MentorSkillsApplication.java
@@ -0,0 +1,14 @@
+package com.mentorskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorSkillsApplication.class, args);
+	}
+
+}
+
