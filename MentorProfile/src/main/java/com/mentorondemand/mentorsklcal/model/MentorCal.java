b4da9b799a9cce6e56457e888dd3e28f94952e2d
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-31 17:08:10 +0530

    Updated

--- MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/model/MentorCal.java
+++ MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/model/MentorCal.java
@@ -4,6 +4,9 @@
 package com.mentorondemand.mentorsklcal.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -15,8 +18,12 @@ import javax.persistence.Table;
 @Table(name = "MentorCal")
 public class MentorCal {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	/*
-	 * id mid start_time end_time start_date end_date
+	 *  mid start_time end_time start_date end_date
 	 */
 
 
