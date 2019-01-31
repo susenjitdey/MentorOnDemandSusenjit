dda505ff758a71ff29c25561726d824664f1f105
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-01-31 16:50:35 +0530

    Added and updated code

--- MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/repositorydao/MentorCalRepositoryDao.java
+++ MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/repositorydao/MentorCalRepositoryDao.java
@@ -3,10 +3,16 @@
  */
 package com.mentorondemand.mentorsklcal.repositorydao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mentorondemand.mentorsklcal.model.MentorCal;
+
 /**
  * @author javaibm04
  *
  */
-public class MentorCalRepositoryDao {
+@Repository
+public interface MentorCalRepositoryDao extends JpaRepository<MentorCal, Long>{
 
 }
--- MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/repositorydao/MentorSkillRepositoryDao.java
+++ MentorProfile/src/main/java/com/mentorondemand/mentorsklcal/repositorydao/MentorSkillRepositoryDao.java
@@ -3,10 +3,16 @@
  */
 package com.mentorondemand.mentorsklcal.repositorydao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mentorondemand.mentorsklcal.model.MentorSkill;
+
 /**
  * @author javaibm04
  *
  */
-public class MentorSkillRepositoryDao {
+@Repository
+public interface MentorSkillRepositoryDao extends JpaRepository<MentorSkill, Long>{
 
 }
