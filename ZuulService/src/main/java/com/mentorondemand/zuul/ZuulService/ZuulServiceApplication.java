3376f78854382abe8cb2e27422a491392b861668
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-02-06 14:35:16 +0530

    Added new project as service for Zuul Gateway

---
+++ ZuulService/src/main/java/com/mentorondemand/zuul/ZuulService/ZuulServiceApplication.java
@@ -0,0 +1,18 @@
+package com.mentorondemand.zuul.ZuulService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
+
