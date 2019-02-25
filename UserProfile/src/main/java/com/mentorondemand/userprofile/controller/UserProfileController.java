a28deb21ddbbfa5e018fe543dd9dab080f5d6457
javaibm04 <javaibm04@javaibm11.iiht.tech>
2019-02-25 17:20:40 +0530

    Code updated for rest services crete delete etc for User

--- UserProfile/src/main/java/com/mentorondemand/userprofile/controller/UserProfileController.java
+++ UserProfile/src/main/java/com/mentorondemand/userprofile/controller/UserProfileController.java
@@ -5,10 +5,16 @@ package com.mentorondemand.userprofile.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +29,10 @@ import com.mentorondemand.userprofile.repositorydao.UserRepositorydao;
 @RestController
 @RequestMapping("/user")
 public class UserProfileController {
-	
+
 	@Autowired
 	UserRepositorydao userRepositorydao;
-	
+
 	@GetMapping("/allUsers")
 	public List<UserProfileEntity> getAllUsers() {
 		System.out.println("get all Users");
@@ -36,26 +42,85 @@ public class UserProfileController {
 		return userProfileEntities;
 
 	}
-	
+
 	@PostMapping(value = "/user/create")
 	public UserProfileEntity createUser(@RequestBody UserProfileEntity userProfileEntity) {
 
+		System.out.println("Creating User::::");
 		UserProfileEntity _userProfileEntity = userRepositorydao.save(new UserProfileEntity(
 				userProfileEntity.getActive(), userProfileEntity.getContactnumber(), userProfileEntity.getFirstname(),
 				userProfileEntity.getLastname(), userProfileEntity.getPassword(), userProfileEntity.getReg_code(),
 				userProfileEntity.getReg_datetime(), userProfileEntity.getUsername()));
-		// Customer _customer = repository.save(new Customer(customer.getName(),
-		// customer.getAge()));
+
 		return _userProfileEntity;
+	}
+
+	@GetMapping("/getUser/{id}")
+	public ResponseEntity<UserProfileEntity> findUserById(@PathVariable("id") long id) {
+		System.out.println("Fetching User Details::::");
+
+		Optional<UserProfileEntity> userProfileEntities = userRepositorydao.findById(id);
+
+		if (userProfileEntities.isPresent()) {
+			UserProfileEntity _userProfileEntities = userProfileEntities.get();
+
+			return new ResponseEntity<>(_userProfileEntities, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@PutMapping("/user/update/{id}")
+	public ResponseEntity<UserProfileEntity> updateUser(@PathVariable("id") long id,
+			@RequestBody UserProfileEntity userProfileEntity) {
+		System.out.println("Update User with ID = " + id + "...");
+
+		Optional<UserProfileEntity> userProfileEntities = userRepositorydao.findById(id);
+
+		if (userProfileEntities.isPresent()) {
+			UserProfileEntity _userProfileEntitys = userProfileEntities.get();
+			_userProfileEntitys.setActive(userProfileEntity.getActive());
+			_userProfileEntitys.setContactnumber(userProfileEntity.getContactnumber());
+			_userProfileEntitys.setFirstname(userProfileEntity.getFirstname());
+			_userProfileEntitys.setLastname(userProfileEntity.getLastname());
+			_userProfileEntitys.setReg_code(userProfileEntity.getReg_code());
+			_userProfileEntitys.setPassword(userProfileEntity.getPassword());
+			_userProfileEntitys.setUsername(userProfileEntity.getUsername());
+
+			return new ResponseEntity<>(_userProfileEntitys, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/user/delete/{id}")
+	public ResponseEntity<UserProfileEntity> deleteUser(@PathVariable("id") long id,
+			@RequestBody UserProfileEntity userProfileEntity) {
+		System.out.println("Delete User with ID = " + id + "...");
+		//
+		Optional<UserProfileEntity> userProfileEntities = userRepositorydao.findById(id);
+		
+			userRepositorydao.deleteById(id);
+			return new ResponseEntity<>(userProfileEntity, HttpStatus.OK);
+
+			
+	}
+
+	@GetMapping("/blockUser")
+	public List<UserProfileEntity> blockUser() {
+
+		return userRepositorydao.findAll();
+
 	}
 
 	@GetMapping("/testString")
 	public String getTestString() {
-		
+
 		return "Hello USerAm Working";
-		
+
 	}
-	
+
 	/*
 	 * @GetMapping("/login") public List<UserProfileEntity> login() {
 	 * 
@@ -63,41 +128,27 @@ public class UserProfileController {
 	 * 
 	 * }
 	 */
-	
-	@GetMapping("/getUser")
-	public List<UserProfileEntity> getUser() {
-		
-		return userRepositorydao.findAll();
-		
-	}
-	
-	@GetMapping("/signup")
-	public List<UserProfileEntity> signup() {
-		
-		return userRepositorydao.findAll();
-		
-	}
-	
-	@GetMapping("/confirmUser")
-	public List<UserProfileEntity> confirmUser() {
-		
-		return userRepositorydao.findAll();
-		
-	}
-	
-	@GetMapping("/blockUser")
-	public List<UserProfileEntity> blockUser() {
-		
-		return userRepositorydao.findAll();
-		
-	}
-	
-//input
-	//id
-	
-	//output
-	//UserDetails
-	//No usage of other microservices.
-	
+
+	/*
+	 * @GetMapping("/login") public List<UserProfileEntity> login() {
+	 * 
+	 * return userRepositorydao.findAll();
+	 * 
+	 * }
+	 */
+
+	/*
+	 * @GetMapping("/signup") public List<UserProfileEntity> signup() {
+	 * 
+	 * return userRepositorydao.findAll();
+	 * 
+	 * }
+	 * 
+	 * @GetMapping("/confirmUser") public List<UserProfileEntity> confirmUser() {
+	 * 
+	 * return userRepositorydao.findAll();
+	 * 
+	 * }
+	 */
 
 }
