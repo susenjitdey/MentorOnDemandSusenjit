10097d395665658fd899a035b5114b9b20a3db08
susenjit <susenjitdey@gmail.com>
2019-03-11 14:07:18 +0530

    Modified code

---
+++ SpringBootJwtAuthentication/src/main/java/com/menondemand/jwtauthentication/mail/MailSender.java
@@ -0,0 +1,26 @@
+package com.menondemand.jwtauthentication.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component("mentorMailSender")
+public class MailSender {
+	
+	@Autowired
+	JavaMailSender javaMailSender;
+	
+	public void sendMail(String from, String to, String subject, String body) {
+		
+		SimpleMailMessage mail = new SimpleMailMessage();
+ 
+		mail.setFrom(from);
+		mail.setTo(to);
+		mail.setSubject(subject);
+		mail.setText(body);
+		
+		javaMailSender.send(mail);
+	}
+
+}
--- SpringBootJwtAuthentication/src/main/java/com/menondemand/jwtauthentication/repository/MentorRepository.java
+++ SpringBootJwtAuthentication/src/main/java/com/menondemand/jwtauthentication/repository/MentorRepository.java
@@ -13,12 +13,11 @@ import com.ibm.mentor.model.Mentor;
 
 @Repository
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
-    Optional<Mentor> findByUsername(String username);
+	
+	
+    Optional<Mentor> findByUsername();
     Boolean existsByUsername(String username);
     List<Mentor> findBySkillsNameContainingAndStartTimeLessThanAndEndTimeGreaterThan(String name, int startTime, int endTime);
     
-//    @Query("SELECT a.username, a.years_exp FROM mentor a, mentor_skills b, \r\n" + 
-//    		"skills c where a.id = b.mentor_id and b.skill_id = c.id \r\n" + 
-//    		"and c.name = (:skillName)")
-//    List<Mentor> findBySkill(@Param("skillName") String skillName);
+
 }
